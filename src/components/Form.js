1c589bfa43b11fc5c7758ef041ea5efe6fb693d6
Yaser Odat <yaserodat@outlook.com>
2022-07-31 23:15:18 +0300

    fixed an error

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './style.css';
