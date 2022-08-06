af01b3d5c5ae18f6620cf988c575d68b1dc720ab
Yaser Odat <yaserodat@outlook.com>
2022-08-06 16:32:47 +0300

    added proper gitignore content, try, catch

---
+++ src/components/Error.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Alert from 'react-bootstrap/Alert';
+
+export default class Error extends React.Component {
+
+    render() {
+        return (
+            <Alert variant="success">
+                <Alert.Heading>Sorry</Alert.Heading>
+                <p>
+                The city you are looking for doesn't exist in our database
+                </p>
+                <hr />
+                <p className="mb-0">
+                   Try to search for a new city.
+                </p>
+            </Alert>
+        )
+    }
+}
